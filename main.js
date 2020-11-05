ee80e8f631f1e1dc8576d752ddba4af291387254
bobbyg211 <robertdg96@gmail.com>
2020-11-04 23:08:26 -0600

    Type compare #1

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const getAllPokemon = () => {
+// Initilization
+let firstPokeSelect = document.querySelector(".first .all-pokemon");
+let secondPokeSelect = document.querySelector(".second .all-pokemon");
+
+// Functions
+const getAllPokemon = (selector) => {
   
   let prom = fetch("https://pokeapi.co/api/v2/generation/1");
   let data = prom.then(function(res) {
@@ -13,22 +18,14 @@ const getAllPokemon = () => {
       let option = document.createElement("option");
       option.innerText = pokeName;
 
-      document.getElementById("all-pokemon").appendChild(option);
+      document.querySelector(selector + " .all-pokemon").appendChild(option);
     });
   });
 
 }
 
-getAllPokemon();
-
-let pokeSelect = document.getElementById("all-pokemon");
-
-pokeSelect.addEventListener("change", function() {
-  pokeDisplay();
-});
-
-const pokeDisplay = () => {
-  let API = "https://pokeapi.co/api/v2/pokemon/" + pokeSelect.value;
+const pokeDisplay = (selector, value) => {
+  let API = "https://pokeapi.co/api/v2/pokemon/" + value;
 
   let prom = fetch(API);
   let data = prom.then(function(res) {
@@ -39,17 +36,52 @@ const pokeDisplay = () => {
 
     // Get and display sprite
     let pokeImgURL = json.sprites.front_default;
-    document.getElementById("poke-img").src = pokeImgURL;
+    document.querySelector(selector + " .poke-img").src = pokeImgURL;
 
     // get and display typing
     let pokeTypes = json.types;
-    document.getElementById("types").innerHTML = "";
+    document.querySelector(selector + " .types").innerHTML = "";
     pokeTypes.forEach(function(types) {
       let type = types.type.name;
       let li = document.createElement("li");
       li.innerText = type;
       li.className = "type " + type;
-      document.getElementById("types").appendChild(li);
+      document.querySelector(selector + " .types").appendChild(li);
     });
   });
 }
+
+const compareTypes = () => {
+  let poke1TypesArray = [];
+  let poke2TypesArray = [];
+
+  let poke1Types = document.querySelector(".first .types").childNodes;
+  poke1Types.forEach(function(type) {
+    poke1TypesArray.push(type.innerText);
+  });
+
+  let poke2Types = document.querySelector(".second .types").childNodes;
+  poke2Types.forEach(function(type) {
+    poke2TypesArray.push(type.innerText);
+  });
+
+  console.log(poke1TypesArray);
+  console.log(poke2TypesArray);
+}
+
+// Usage
+getAllPokemon(".first");
+getAllPokemon(".second");
+
+firstPokeSelect.addEventListener("change", function() {
+  let val = firstPokeSelect.value;
+  pokeDisplay(".first", val);
+  setTimeout(function(){ compareTypes(); }, 1000);
+});
+
+secondPokeSelect.addEventListener("change", function() {
+  let val = secondPokeSelect.value;
+  pokeDisplay(".second", val);
+  setTimeout(function(){ compareTypes(); }, 1000);
+});
+
